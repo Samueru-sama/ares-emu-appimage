4a7bbfc901007bb20fca9aef49da3d3106f62e06
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-11 10:28:57 -0400

    test without debloated

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -64,7 +64,7 @@ wget --retry-connrefused --tries=30 "$LLVM_URL" -O   ./llvm-libs.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$OPUS_URL" -O   ./opus-nano.pkg.tar.zst
 
-pacman -U --noconfirm ./*.pkg.tar.zst
+#pacman -U --noconfirm ./*.pkg.tar.zst
 rm -f ./*.pkg.tar.zst
 
 # Make librashader
